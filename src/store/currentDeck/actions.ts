5085c091a0bd41a8fae2612124fd9d3d3dec997d
Riccardo Causo <rixlabs@gmail.com>
2018-10-19 15:46:41 +0200

    fixed flickering missing permission and cathced error

--- src/store/currentDeck/actions.ts
+++ src/store/currentDeck/actions.ts
@@ -3,20 +3,33 @@ import { ActionTree } from 'vuex';
 import { RootState } from '../types';
 import { decksCollection } from '../../config/db';
 import { CurrentDeckState } from './types';
+import firebase from 'firebase';
+import _ from 'lodash';
 
 
 export const actions: ActionTree<CurrentDeckState, RootState> = {
     setCurrentDeck({ commit }, deckId: string) {
-        decksCollection.doc(deckId).onSnapshot((doc: any) => {
-            commit('SET_CURRENT_DECK', { id: deckId, ...doc.data() });
+        decksCollection.doc(deckId).onSnapshot({ includeMetadataChanges: true }, (doc: any) => {
+            const source = doc.metadata.hasPendingWrites ? 'Local' : 'Server';
+            if (source === 'Server') {
+                commit('SET_CURRENT_DECK', { id: deckId, ...doc.data() });
+            }
         });
     },
     addCardToCurrentDeck({ commit }, newCard: any) {
+        if (_.isEmpty(newCard.data.front) || _.isEmpty(newCard.data.back) || _.isEmpty(newCard.deckId)) {
+            throw new Error('Card fields cannot be empty');
+            return;
+        }
         decksCollection.doc(newCard.deckId).get().then((deck: any) => {
             const cardsRef = deck.data().cards;
             const newCards = JSON.parse(JSON.stringify(cardsRef));
             newCards.push(newCard.data);
-            decksCollection.doc(newCard.deckId).update({ cards: newCards }).catch((err) => undefined);
+            decksCollection.doc(newCard.deckId).update({
+                cards: firebase.firestore.FieldValue.arrayUnion(newCard.data),
+            }).catch((err: any) => {
+                return undefined;
+            });
         });
     },
 };
